2e983e4a1a46e0638aaf36afb0a7c018b6f6f63a
David Tays <davidtays22@gmail.com>
2018-07-29 19:10:03 -0500

    week-3.2

--- week-3/guards/src/app/app.module.ts
+++ week-3/guards/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
