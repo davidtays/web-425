1d194ed26c0e25e99196837a30906b543ed60f54
David Tays <davidtays22@gmail.com>
2018-09-04 20:37:08 -0500

    week 8-2

--- week-8/tays-ts-server/app.ts
+++ week-8/tays-ts-server/app.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 
 const app = express();
-interface Composer{
+
+interface Composer {
     id: number,
     firstName: string,
-    lastName: string,
+    lastName: string
 }
 
 const composers: Composer[] = [
@@ -15,24 +16,24 @@ const composers: Composer[] = [
     { id: 5, firstName: "Frederic", lastName: "Chopin"},
 ];
 
-function getComposers(): Composer[]{
+function getComposers(): Composer[] {
     return composers;
 }
 
-app.get('/', (req, res) => {
-    res.send('The Url for composers is http://localhost:3000/api/composers');
+app.get('/', (request, response) => {
+    response.send('The URL for composers is http://localhost:3000/api/composers');
 });
 
-app.get('/api/composers', (req, res) => {
-    res.json(getComposers());
+app.get('/api/composers', (request, response) => {
+    response.json(getComposers());
 });
 
 function getComposerById(composerId: number): Composer {
     return composers.find(c => c.id == composerId);
 }
 
-app.get('/api/composer/:id', (req, res) => {
-    res.json(getComposerById(req.params.id));
+app.get('/api/composer/:id', (request, response) => {
+    response.json(getComposerById(request.params.id));
 });
 
 const server = app.listen(3000, "localhost", () => {
